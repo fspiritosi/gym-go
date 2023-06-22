4129e553fa85e010ca62b99c5fb5dfbb5cd6950d
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-06-22 12:14:41 -0300

    actitivites and goals models with many to many relationship

---
+++ api/src/models/Activities.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define("Activities", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING(1234),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING(1234),
+      allowNull: false,
+    },
+    goals: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAcive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
+  });
+};
---
+++ api/src/models/Goals.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define("Goals", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
+  });
+};

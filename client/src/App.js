9a8dbe78ef437f81b3df6b78c731a524ed01feec
SandyAstorga <sandyastorga13@gmail.com>
2023-06-23 13:49:34 -0600

    se crea el componente landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <Router>
       <div className="App">
       <Routes>
-        
+        <Route exact path="/" element={<Landing />} />
       </Routes>
       </div>
     </Router>

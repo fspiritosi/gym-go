e542ab89347023781cce6aa04a2b6bb54ddf6a31
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-06-24 22:13:57 -0300

    defining route paths and useLocation hook for Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './components/Landing/Landing';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Landing from './views/Landing/Landing';
+import Home from './views/Home/Home';
+import Detail from './views/Detail/Detail';
+import Form from './views/Form/Form';
+import Nav from './components/Nav/Nav';
 
 function App() {
+  let location = useLocation();
+
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      {location.pathname !== "/" && <Nav />}
       <Routes>
         <Route exact path="/" element={<Landing />} />
+        <Route path="/activities" element={<Home />} />
+        <Route path="/activity-detail" element={<Detail />} />
+        <Route path="/create-activity" element={<Form />} />
       </Routes>
-      </div>
-    </Router>
+    </div>
   );
 }
 

735a14eb9644f4cce4a743b3a03d25af6cb2161c
fspiritosi <fspiritosi@gmail.com>
2023-06-26 00:12:09 -0300

    Cargando el formilario de Creación de Actividades

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+<<<<<<< Updated upstream
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Landing from './views/Landing/Landing';
 import Home from './views/Home/Home';
 import Detail from './views/Detail/Detail';
 import Form from './views/Form/Form';
 import Nav from './components/Nav/Nav';
+=======
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
+import FormCreateActivities from './components/FormCreateActivities/FormCreateActivities';
+>>>>>>> Stashed changes
 
 function App() {
   let location = useLocation();
 
   return (
+<<<<<<< Updated upstream
     <div className="App">
       {location.pathname !== "/" && <Nav />}
       <Routes>
@@ -19,6 +26,16 @@ function App() {
         <Route path="/create-activity" element={<Form />} />
       </Routes>
     </div>
+=======
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/createActivity" element={<FormCreateActivities />} />
+        </Routes>
+      </div>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 

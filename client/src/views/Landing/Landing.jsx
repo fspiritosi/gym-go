11166de37f04443ec6e9a1d7bbc0277425b0e230
dayhana61096 <dayhanaossa@gmail.com>
2023-06-26 12:10:41 -0500

    segundos cambios en estilos landing

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './image/logo.png';
 import styles from './Landing.module.css';
+import {Link} from 'react-router-dom'
 
 
 const Landing = () => {
@@ -9,9 +10,11 @@ const Landing = () => {
         <div className={styles.container}>
             <img className={styles.img} src={logo} alt='logo'/>
             <h1 className={styles.text}>Comienza el Reto</h1>
-            <button className={styles.button}>
+            <Link to='/activities'>
+            <button  className={styles.button}>
                 Home
             </button>
+            </Link>
         </div>
     )
 }

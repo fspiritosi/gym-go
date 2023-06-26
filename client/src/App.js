06a9d3a7d57f0583c03ae13ff04fafa9725c4e1e
Fabricio Spiritosi <35983630+fspiritosi@users.noreply.github.com>
2023-06-26 09:41:03 -0300

    Update App.js
    reparando error de merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,28 @@
 import './App.css';
-<<<<<<< Updated upstream
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './views/Landing/Landing';
 import Home from './views/Home/Home';
 import Detail from './views/Detail/Detail';
-import Form from './views/Form/Form';
 import Nav from './components/Nav/Nav';
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
-import FormCreateActivities from './components/FormCreateActivities/FormCreateActivities';
->>>>>>> Stashed changes
+import FormCreateActivities from './views/FormCreateActivities/FormCreateActivities';
+
 
 function App() {
   let location = useLocation();
 
   return (
-<<<<<<< Updated upstream
+
     <div className="App">
       {location.pathname !== "/" && <Nav />}
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/activities" element={<Home />} />
         <Route path="/activity-detail" element={<Detail />} />
-        <Route path="/create-activity" element={<Form />} />
+        <Route path="/create-activity" element={<FormCreateActivities />} />
       </Routes>
     </div>
-=======
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route path="/createActivity" element={<FormCreateActivities />} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> Stashed changes
+
   );
 }
 

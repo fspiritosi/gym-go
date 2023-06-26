7ef9d941030c787fe2ad7142e435b47eeaf68e42
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-06-25 22:01:35 -0300

    remove attribute goals from activities model and alter: true in index.js

--- api/src/models/Activities.js
+++ api/src/models/Activities.js
@@ -20,10 +20,6 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING(1234),
       allowNull: false,
     },
-    goals: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
     difficulty: {
       type: DataTypes.ENUM,
       values: ['easy', 'medium', 'hard'],

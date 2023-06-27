d7064bd4fa0c8a0245c9fe9b9b3a43b7bf9d2239
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-06-26 22:27:27 -0300

    Adding style to CardActivities and Activities components

--- client/src/components/Activities/Activities.jsx
+++ client/src/components/Activities/Activities.jsx
@@ -3,35 +3,39 @@ import CardActivities from '../CardActivities/CardActivities';
 import SearchBar from '../SearchBar/SearchBar';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getActivities } from '../../redux/actions'; 
+import { getActivities } from '../../redux/actions';
+import styles from './Activities.module.css';
 
 const Activities = () => {
-    
-    const dispatch = useDispatch();
-    const activities = useSelector((state) => state.activities);
+  const dispatch = useDispatch();
+  const activities = useSelector((state) => state.activities);
 
-    useEffect(() => {
-        dispatch(getActivities());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
 
-    return(
+  return(
+    <div>
+      <h1 className={styles.title}>Actividades</h1>
         <div>
-            <h1>Activities</h1>
-            <div>
-                <SearchBar />
-                {activities?.map((a,index) => {
-                    return (
-            <CardActivities
-                key={index}
-                image={a.image}
-                title={a.title}
-                difficulty={a.difficulty}
-                />
-                )
-            })}
-            </div>
+          <SearchBar />
         </div>
-    )
+        <div className={styles.activitiesContainer}>
+          {activities?.map((activity,index) => {
+            return (
+              <CardActivities
+                key={index}
+                id={activity.id}
+                image={activity.image}
+                title={activity.title}
+                difficulty={activity.difficulty}
+                goals={activity.Goals}
+              />
+            )
+          })}
+        </div>
+    </div>
+  )
 };
 
 export default Activities;
--- client/src/components/CardActivities/CardActivities.jsx
+++ client/src/components/CardActivities/CardActivities.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CardActivities.module.css'
 
-const Card = ({ title, image }) => {
+const Card = ({ id, title, image, difficulty, goals }) => {
 
     return (
-        <div>
-            <div>
-            <h5>{image}</h5>
-            <h2>{title}</h2>
-            <button>Detail</button>
-            </div>
+        <div className={styles.card}>
+            <img src={image} alt={title} />
+            <Link to={`/activity-detail/${id}`} key={id} className={styles.cardLink}>
+                <h2>{title}</h2>
+            </Link>
+            {/* <p>{difficulty}</p>
+            <ul>
+                {goals.map(goal => (
+                    <li key={goal}>{goal}</li>
+                ))}
+            </ul> */}
         </div>
     );
 };

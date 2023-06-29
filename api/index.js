41010cf5bddbec0bbb40235381fb01b23dd1a148
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-06-29 17:19:39 -0300

    force: true

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });

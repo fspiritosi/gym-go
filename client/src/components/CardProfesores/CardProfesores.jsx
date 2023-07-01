98b275aeccbf5390be610a9af4c6765d78f5be8f
SandyAstorga <sandyastorga13@gmail.com>
2023-07-01 16:23:40 -0600

    update coaches

--- client/src/components/CardProfesores/CardProfesores.jsx
+++ client/src/components/CardProfesores/CardProfesores.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './CardProfesores.module.css'
 
 
 const Card = ({ id, firstName, profilePicture, description, lastName }) => {
 
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
     return (
-        <div>
+        <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={handleFlip}>
+            <div className={styles.cardfront}>
             <h2>{firstName}{lastName}</h2>
-            <img src={profilePicture} alt='' className={styles.profile}/>
+            <img src={profilePicture} alt='' className={styles.profile} />
+            </div>
+            <div className={styles.cardback}>
             <p>{description}</p>
+            </div>
         </div>
     );
 };

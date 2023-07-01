c25d072748f509806a400c622da154330f9a202f
Dac1977 <diegodac77@gmail.com>
2023-07-01 01:20:13 -0300

    Auth0 implementado en navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
-import './App.css';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Landing from './views/Landing/Landing';
-import Home from './views/Home/Home';
-import Detail from './views/Detail/Detail';
-import Nav from './components/Nav/Nav';
-import FormCreateActivities from './views/FormCreateActivities/FormCreateActivities';
-import axios from 'axios'
-import FormGoals from './views/FormGoals/FormGoals'
-axios.defaults.baseURL = "http://localhost:3001"
-
-
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Landing from "./views/Landing/Landing";
+import Home from "./views/Home/Home";
+import Detail from "./views/Detail/Detail";
+import Nav from "./components/Nav/Nav";
+import FormCreateActivities from "./views/FormCreateActivities/FormCreateActivities";
+import axios from "axios";
+import FormGoals from "./views/FormGoals/FormGoals";
+axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
   let location = useLocation();
 
   return (
-
     <div className="App">
       {location.pathname !== "/" && <Nav />}
+
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/activities" element={<Home />} />
         <Route path="/activity-detail/:id" element={<Detail />} />
         <Route path="/create-activity" element={<FormCreateActivities />} />
-        <Route path='/create-goals' element={<FormGoals/>}/>
+        <Route path="/create-goals" element={<FormGoals />} />
       </Routes>
     </div>
-
   );
 }
 
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import { Link} from "react-router-dom";
-import Logo from '../../views/Landing/image/logo.png'
-import "./Navbar.css"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../views/Landing/image/logo.png";
+import "./Navbar.css";
+import LoginButton from "../Login/LoginButton";
+import LogoutButton from "../Login/LogoutButton";
+import Profile from "../Login/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 const Nav = () => {
-
-  const[isMobile, setIsMovile] = useState(false);
-    return (
-      <nav className="navbar">
-        {/* <h1 className="text-center">GYM GO</h1> */}
-        <div className='nav-container'>
-
-        <img src={Logo} alt='logo' className="logo" />
+  const [isMobile, setIsMovile] = useState(false);
+  const { isAuthenticated } = useAuth0();
+  return (
+    <nav className="navbar">
+      {/* <h1 className="text-center">GYM GO</h1> */}
+      <div className="nav-container">
+        <img src={Logo} alt="logo" className="logo" />
         <ul
           //   className={isMobile ? "nav-links-mobile" : "nav-links"}
           className="nav-links"
@@ -23,20 +25,34 @@ const Nav = () => {
           <Link to="/create-activity/" className="create-activity">
             <li>Create Classes</li>
           </Link>
+          <li>
+            {isAuthenticated ? (
+              <>
+                <li>
+                  <Profile />
+                </li>
+                <li>
+                  <LogoutButton />
+                </li>
+              </>
+            ) : (
+              <LoginButton />
+            )}
+          </li>
         </ul>
-        </div>
-        <button
-          className="mobile-menu-icon"
-          onClick={() => setIsMovile(!isMobile)}
-        >
-          {isMobile ? (
-            <i className="fas fa-times"></i>
-          ) : (
-            <i className="fas fa-bars"></i>
-          )}
-        </button>
-      </nav>
-    );
-}
+      </div>
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMovile(!isMobile)}
+      >
+        {isMobile ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </button>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;

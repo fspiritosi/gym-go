3e49d324ce10b8a2c505c5bc96828071830825fe
marbello1973 <david.marbello@outlook.com>
2023-07-01 23:01:46 -0500

    'user'

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const activitiesRouter = require("./activitiesRouter");
 const goalsRouter = require("./goalsRouter");
+const userRouter = require("./userRouter");
 
 const mainRouter = Router();
 
 mainRouter.use("/activities", activitiesRouter);
 mainRouter.use("/goals", goalsRouter);
+mainRouter.use("/", userRouter);
 
 module.exports = mainRouter;

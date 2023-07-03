5dc8a56ab89aa11b83d94c2cf79a7e859419b153
Dac1977 <diegodac77@gmail.com>
2023-07-03 09:58:39 -0300

    arreglos de app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./views/Landing/Landing";
@@ -6,23 +5,16 @@ import Home from "./views/Home/Home";
 import Detail from "./views/Detail/Detail";
 import Nav from "./components/Nav/Nav";
 import FormCreateActivities from "./views/FormCreateActivities/FormCreateActivities";
-import Profesores from './views/Profesores/Profesores';
+import Profesores from "./views/Profesores/Profesores";
 import axios from "axios";
 import FormGoals from "./views/FormGoals/FormGoals";
 import Footer from "./components/Footer/Footer";
 axios.defaults.baseURL = "http://localhost:3001";
-=======
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './components/Landing/Landing';
-import FormCreateActivities from './components/FormCreateActivities/FormCreateActivities';
->>>>>>> Stashed changes
 
 function App() {
   let location = useLocation();
 
   return (
-<<<<<<< Updated upstream
     <div className="App">
       {location.pathname !== "/" && <Nav />}
 
@@ -31,22 +23,12 @@ function App() {
         <Route path="/activities" element={<Home />} />
         <Route path="/activity-detail/:id" element={<Detail />} />
         <Route path="/create-activity" element={<FormCreateActivities />} />
-        <Route path='/create-goals' element={<FormGoals/>}/>
-        <Route path='/coaches' element={<Profesores/>}/>
+        <Route path="/create-goals" element={<FormGoals />} />
+        <Route path="/coaches" element={<Profesores />} />
         <Route path="/create-goals" element={<FormGoals />} />
       </Routes>
       {location.pathname !== "/" && <Footer />}
     </div>
-=======
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route path="/createActivity" element={<FormCreateActivities />} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> Stashed changes
   );
 }
 

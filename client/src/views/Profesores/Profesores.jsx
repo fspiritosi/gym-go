b1a3a5c3f38f74b88d05b9d7a4af68d30b7a3194
SandyAstorga <sandyastorga13@gmail.com>
2023-07-03 14:01:13 -0600

    update profesores

--- client/src/views/Profesores/Profesores.jsx
+++ client/src/views/Profesores/Profesores.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import CardProfesores from '../../components/CardProfesores/CardProfesores'
-// import { getCoaches } from '../../redux/actions';
-import Ejemplos from './ejemploprofesores.json'
+import { getCoaches } from '../../redux/actions';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import Ejemplos from './ejemploprofesores.json'
 
 const Profesores = () => {
+
+const dispatch = useDispatch();
+const coaches= useSelector((state) => state.coaches);
+
+useEffect(() => {
+    dispatch(getCoaches());
+}, [dispatch]);
+
     return (
         <div>
-            {/* <h1>Profesores</h1> */}
+            <br/>
+            <h1>Nuestros Profesores</h1>
             <br/>
             <div>
-                {Ejemplos.map((c, index) => {
+                {coaches.map((c, index) => {
                     return ( 
                         <CardProfesores
                             key={index}

96851a4108dc58c7f947d74306ab5c4eba668e71
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-07-04 16:10:05 -0300

    modularizando mercado pago

--- client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
+++ client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
 import style from "./CardPaquete.module.css";
-import axios from 'axios';
-import { initMercadoPago } from '@mercadopago/sdk-react';
 import MercadoPago from '../MercadoPago/MercadoPago';
 
-// const { PUBLIC_KEY } = process.env;
-initMercadoPago("TEST-7951ec00-5a14-4772-b454-928cd10f2a74");
-
 const CardPaquete = ({ title, clases, price, comprar }) => {
-    const [preferenceId, setPreferenceId] = useState(null);
     const [orderData, setOrderData] = useState({ description: title, price: Number(price), quantity: 1 });
+    const [showMercadoPago, setShowMercadoPago] = useState(false);
 
-    const handleBuy = async () => {
-        try {
-            const response = await axios.post("http://localhost:3001/mercadopago/create_preference", orderData);
-            const id = response.data.preferenceId;
-            setPreferenceId(id);
-            return preferenceId;
-          } catch (error) {
-            alert(error);
-          }
+    const handleBuy = () => {
+        setShowMercadoPago(true);
     };
     
     return (
@@ -34,7 +22,7 @@ const CardPaquete = ({ title, clases, price, comprar }) => {
                 className={style.comprar}
                 onClick={handleBuy}
             >Comprar</button>
-            {preferenceId && <MercadoPago preferenceId={preferenceId}/>}
+            {showMercadoPago && <MercadoPago orderData={orderData} />}
             <br/>
         </div>
     );

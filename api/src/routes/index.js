8d5366e9499904c7321e73175ce0c6de59e328f6
Fabricio Spiritosi <35983630+fspiritosi@users.noreply.github.com>
2023-07-04 17:37:21 -0300

    Update index.js
    Cargando Ruta Coaches en /routes/index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const activitiesRouter = require("./activitiesRouter");
 const goalsRouter = require("./goalsRouter");
 const userRouter = require("./userRouter");
+const coachesRouter = require("./coachesRouter");
 const classesRouter = require("./classesRouter");
 
 
@@ -10,6 +11,7 @@ const mainRouter = Router();
 mainRouter.use("/activities", activitiesRouter);
 mainRouter.use("/goals", goalsRouter);
 mainRouter.use("/", userRouter);
+mainRouter.use("/coaches", coachesRouter);
 mainRouter.use("/classes", classesRouter);
 
 

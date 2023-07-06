3c4d3c19b567dd69403969d2b53555800d738488
Dac1977 <diegodac77@gmail.com>
2023-07-06 11:45:42 -0300

    pk de User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -5,7 +5,7 @@ module.exports = (sequelize) => {
   const User = sequelize.define("User", {
     id: {
       type: DataTypes.UUID,
-      // primaryKey: true,
+      primaryKey: true,
       defaultValue: DataTypes.UUIDV4,
     },
     username: {

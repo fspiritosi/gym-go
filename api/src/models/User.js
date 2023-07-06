5dc73116eead1308f8bfa6dd06c1371897d95eab
fspiritosi <fspiritosi@gmail.com>
2023-07-06 10:47:18 -0300

    id modificado

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -5,8 +5,8 @@ module.exports = (sequelize) => {
   const User = sequelize.define("User", {
     id: {
       type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUID,
+      // primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
     username: {
       type: DataTypes.STRING,

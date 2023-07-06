18216b4cf5cb8861d88ea9de16e52dc8b9e13c73
fspiritosi <fspiritosi@gmail.com>
2023-07-06 09:53:31 -0300

    modificando el User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -11,58 +11,59 @@ module.exports = (sequelize) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        notNull: {
-          msg: "Nombre de usuario requerido",
-        },
-        notEmpty: {
-          msg: "El nombre de usuario no puede estar vacio",
-        },
-        len: {
-          args: [3 - 50],
-          msg: "Debe ser mayor a 3 caracteres ",
-        },
-      },
+      // validate: {
+      //   notNull: {
+      //     msg: "Nombre de usuario requerido",
+      //   },
+      //   notEmpty: {
+      //     msg: "El nombre de usuario no puede estar vacio",
+      //   },
+      //   len: {
+      //     args: [3 - 50],
+      //     msg: "Debe ser mayor a 3 caracteres ",
+      //   },
+      // },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: {
-        msg: "El correo electronico ya esta en uso",
-      },
-      validate: {
-        notNull: {
-          msg: "El correo electronico requerido",
-        },
-        notEmpty: {
-          msg: "El correo electronico no puede estar vacio",
-        },
-        isEmail: {
-          msg: "Debe prporcionar un correo electronico valido",
-        },
-        isEmailCustom(value) {
-          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-          if (!emailRegex.test(value)) {
-            throw new Error("El formato del correo electrónico es inválido");
-          }
-        },
-      },
+      unique: true,
+      // {
+      //   msg: "El correo electronico ya esta en uso",
+      // },
+      // validate: {
+      //   notNull: {
+      //     msg: "El correo electronico requerido",
+      //   },
+      //   notEmpty: {
+      //     msg: "El correo electronico no puede estar vacio",
+      //   },
+      //   isEmail: {
+      //     msg: "Debe prporcionar un correo electronico valido",
+      //   },
+      //   isEmailCustom(value) {
+      //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      //     if (!emailRegex.test(value)) {
+      //       throw new Error("El formato del correo electrónico es inválido");
+      //     }
+      //   },
+      // },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        notNull: {
-          msg: "La contraseña es requerida",
-        },
-        notEmpty: {
-          msg: "La contraseña no puede estar vacia",
-        },
-        len: {
-          args: [8, 20],
-          msg: "La contraseña debe tener entre 8 y 20 caracteres",
-        },
-      },
+      // validate: {
+      //   notNull: {
+      //     msg: "La contraseña es requerida",
+      //   },
+      //   notEmpty: {
+      //     msg: "La contraseña no puede estar vacia",
+      //   },
+      //   len: {
+      //     args: [8, 20],
+      //     msg: "La contraseña debe tener entre 8 y 20 caracteres",
+      //   },
+      // },
     },
   });
 

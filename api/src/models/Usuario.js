b074b529988e99955c0330097ea39a0cf875fffc
Dac1977 <diegodac77@gmail.com>
2023-07-06 11:33:41 -0300

    modelo Usuarios

---
+++ api/src/models/Usuario.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const User = sequelize.define("Usuario", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};

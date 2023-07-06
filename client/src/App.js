b632cc34b22d494d283f11988a405b006d06d71c
dayhana61096 <dayhanaossa@gmail.com>
2023-07-06 13:26:21 -0500

    se actualizo la landing page, Navbar y footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./views/Landing/Landing";
-import Home from "./views/Home/Home";
 import Detail from "./views/Detail/Detail";
 import Nav from "./components/Nav/Nav";
 import FormCreateActivities from "./views/FormCreateActivities/FormCreateActivities";
@@ -11,6 +10,7 @@ import axios from "axios";
 import FormGoals from "./views/FormGoals/FormGoals";
 import Footer from "./components/Footer/Footer";
 import FormClasses from "./views/FormClasses/FormClasses";
+import ActivitiesV from "./views/ActivitiesV/ActivitiesV";
 axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
@@ -19,10 +19,9 @@ function App() {
   return (
     <div className="App">
       {location.pathname !== "/" && <Nav />}
-
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route path="/activities" element={<Home />} />
+        <Route path="/activities" element={<ActivitiesV />} />
         <Route path="/activity-detail/:id" element={<Detail />} />
         <Route path="/create-activity" element={<FormCreateActivities />} />
         <Route path='/create-goals' element={<FormGoals/>}/>

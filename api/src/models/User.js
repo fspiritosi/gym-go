9347b781e376b577ebceca0f4a9f77131b452323
fspiritosi <fspiritosi@gmail.com>
2023-07-06 10:32:24 -0300

    cambiando el UUID

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -6,7 +6,7 @@ module.exports = (sequelize) => {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUID,
     },
     username: {
       type: DataTypes.STRING,

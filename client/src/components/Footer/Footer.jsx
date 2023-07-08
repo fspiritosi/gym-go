885c2f1d5c4b248910b1a0eacc9a3b4636ad6608
SandyAstorga <sandyastorga13@gmail.com>
2023-07-07 19:54:44 -0600

    update classes

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -22,20 +22,6 @@ const Footer = () => {
     </div>
      </footer>  
   )
-}
-//Se comento ya que estaba generando error 
-
-//       </div>
-
-//       <p class="mt-4 text-sm text-center text-white lg:text-right lg:mt-0">
-//         T&C &nbsp; Career &nbsp; Privacy & Policy &nbsp; Developers
-//       </p>
-//     </div>
-//     </div>
-//      </footer>   
-//   )
-// }
-
- 
+} 
 
 export default Footer;

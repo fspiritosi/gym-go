4051ccdc81da89942c6903c101a39d3d8583d850
SandyAstorga <sandyastorga13@gmail.com>
2023-07-09 02:11:56 -0600

    update

--- client/src/views/Classes/Classes.jsx
+++ client/src/views/Classes/Classes.jsx
@@ -6,6 +6,49 @@ import { getActivities, getClassess, getCoaches } from '../../redux/actions';
 import CardClasses from "../../components/CardClasses/CardClasses";
 
 
+// const Classes = () => {
+//     const dispatch = useDispatch();
+//     const activities = useSelector((state) => state.activities);
+//     const coaches = useSelector((state) => state.coaches);
+//     // const classes = useSelector((state) => state.classes);
+
+//     useEffect(() => {
+//         dispatch(getActivities());
+//         dispatch(getClassess());
+//         dispatch(getCoaches());
+//     }, [dispatch]);
+
+//     return (
+//         <div>
+//             <br />
+//             <h1>Clases</h1>
+//             <div>
+//                 {activities?.map((activity) => {
+//                     return activity.Classes.map((clase, index) => {
+//                         const coach = coaches.find(coach => coach.id === clase.Coach.id);
+//                         const coachName = coach ? `${coach.firstName} ${coach.lastName}` : '';
+//                         return clase.Events.map((event, index) => (
+//                             <CardClasses
+//                                 key={index}
+//                                 id={activity.id}
+//                                 title={activity.title}
+//                                 coachName={coachName}
+//                                 difficulty={clase.difficulty}
+//                                 date={event.date}
+//                                 startTime={event.startTime}
+//                                 endTime={event.endTime}
+//                                 duration={event.duration}
+//                                 quota={clase.quota}
+//                             />
+//                         ));
+//                     });
+//                 })}
+//             </div>
+//         </div>
+//     )
+
+// };
+
 const Classes = () => {
     const dispatch = useDispatch();
     const activities = useSelector((state) => state.activities);
@@ -21,32 +64,34 @@ const Classes = () => {
     return (
         <div>
             <br />
+            <br />
+            <br />
             <h1>Clases</h1>
-            <div>
+            <br />
+            {/* <div> */}
                 {activities?.map((activity) => {
                     return activity.Classes.map((clase, index) => {
                         const coach = coaches.find(coach => coach.id === clase.Coach.id);
                         const coachName = coach ? `${coach.firstName} ${coach.lastName}` : '';
-                        return clase.Events.map((event, index) => (
+                        return (
                             <CardClasses
                                 key={index}
                                 id={activity.id}
-                                difficulty={clase.difficulty}
                                 title={activity.title}
-                                date={event.date}
-                                startTime={event.startTime}
-                                endTime={event.endTime}
-                                duration={event.duration}
-                                quota={clase.quota}
+                                difficulty={clase.difficulty}
+                                startTime={clase.Events[0].startTime}
+                                endTime={clase.Events[0].endTime}
+                                duration={clase.Events[0].duration}
+                                quota= {clase.quota}
+                                date={clase.Events.map((d) => d.date)}
                                 coachName={coachName}
                             />
-                        ));
+                        );
                     });
                 })}
             </div>
-        </div>
+        // </div>
     )
-
 };
 
 

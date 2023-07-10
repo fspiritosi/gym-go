2c50f9e7efda3108a2d1f73b54e9536c8adcd751
SandyAstorga <sandyastorga13@gmail.com>
2023-07-10 16:25:12 -0600

    update classes por enesima vez

--- client/src/components/CardClasses/CardClasses.jsx
+++ client/src/components/CardClasses/CardClasses.jsx
@@ -10,7 +10,7 @@ import LoginButton from '../../components/Login/LoginButton';
 
 Modal.setAppElement('#root');
 
-const CardClasses = ({ id, title, image, difficulty, CoachId, date, startTime, endTime, eventQuota, quota, duration, coachName }) => {
+const CardClasses = ({ id, title, classes, image, difficulty, CoachId, date, startTime, endTime, eventQuota, quota, duration, coachName }) => {
     const [showModal, setShowModal] = useState(false);
     const [isClassReserved, setIsClassReserved] = useState(false);
     const { isAuthenticated, loginWithRedirect } = useAuth0();
@@ -24,31 +24,26 @@ const CardClasses = ({ id, title, image, difficulty, CoachId, date, startTime, e
         }
     };
 
-    const closeModal = () => {
-        setShowModal(false);
-    };
-
+    const closeModal = () => { setShowModal(false); };
     const handleModalLogin = () => {
         loginWithRedirect(); // Redirige al usuario a la página de auth0
     };
 
     return (
         <div className={styles.cardContainer}>
-                <br />
-                <div>
-                <h4>Actividad: {title}</h4>
-                {/* <img src={image} alt=''/> */}
-                <h4>Profesor: {coachName}</h4>
-                <h4>Horario: {startTime} - {endTime}</h4>
-                <h4>Duracion: {duration} hora(s)</h4>
-                <h4>Cupo: {quota} espacios por evento</h4>
-                <br />
-                </div>
-                <h1>Eventos</h1>
-                <h4>Dificultad: {difficulty}</h4>
-                {date.map((dateItem, index) => (
-                    <div key={index}>
-                        <button onClick={handleReserva} className={style.eventButton}>{dateItem}</button>
+            <h4>Actividad: {title}</h4>
+            <h4>Profesor: {coachName}</h4>
+            <h4>Dificultad: {difficulty}</h4>
+            <h4>Horario: {startTime} - {endTime}</h4>
+            <h4>Duracion: {duration} hora(s)</h4>
+            <h4>Cupo: {quota} espacios por evento</h4>
+            <h1>Eventos</h1>
+            <div className={styles.cardContainer}>
+                {date.map((event) => (
+                    <div key={event.id}>
+                        <button onClick={handleReserva} className={style.eventButton}>
+                            {event}
+                        </button>
                         <br />
                         <Modal
                             isOpen={showModal}
@@ -64,63 +59,8 @@ const CardClasses = ({ id, title, image, difficulty, CoachId, date, startTime, e
                     </div>
                 ))}
             </div>
+        </div>
     );
 };
 
 export default CardClasses;
-
-
-// const CardClasses = ({ id, title, difficulty, CoachId, date, startTime, endTime, eventQuota, quota, duration, coachName }) => {
-//     const [showModal, setShowModal] = useState(false);
-//     const [isClassReserved, setIsClassReserved] = useState(false);
-//     const { isAuthenticated, loginWithRedirect } = useAuth0();
-
-//     const handleReserva = () => {
-//         if (isAuthenticated) {
-//             setShowModal(false)
-//             setIsClassReserved(true); // Se marca la clase como reservada
-//         } else {
-//             setShowModal(true);
-//         }
-//     };
-
-//     const closeModal = () => {
-//         setShowModal(false);
-//     };
-
-//     const handleModalLogin = () => {
-//         loginWithRedirect(); // Redirige al usuario a la página de auth0
-//     };
-
-//     return (
-//         <div className={styles.cardContainer}>
-//             <br />
-//             {date && startTime && endTime && (
-//                 <div>
-//                     <h4>Actividad: {title}</h4>
-//                     <h4>Profesor: {coachName}</h4>
-//                     <h4>Fecha: {date}</h4>
-//                     <h4>Horario: {startTime} - {endTime}</h4>
-//                     <h4>Dificultad: {difficulty}</h4>
-//                     <h4>Duracion: {duration} hora(s)</h4>
-//                     <h4>Cupo: {quota} espacios</h4>
-//                     {/* <h4>Disponibilidad: {eventQuota[index]}</h4> */}
-//                     <br />
-//                     <button onClick={handleReserva}>Suscribirse a Evento</button>
-//                     <Modal
-//                         isOpen={showModal}
-//                         onRequestClose={closeModal}
-//                         className={`${style.modalContent} ${style.modalOverlay}`}
-//                     >
-//                         <h2>Debes iniciar sesión o Registrarte para suscribirte</h2>
-//                         <button onClick={handleModalLogin} className={style.modalButton}>Iniciar sesión</button>
-//                         <button onClick={handleModalLogin} className={style.modalButton}>Registrarse</button>
-//                         <button onClick={closeModal} className={style.modalButton}>Cerrar</button>
-//                     </Modal>
-//                     {isClassReserved && <p>Clase reservada</p>}
-//                     <br />
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
--- client/src/views/Classes/Classes.jsx
+++ client/src/views/Classes/Classes.jsx
@@ -5,55 +5,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getActivities, getClassess, getCoaches } from '../../redux/actions';
 import CardClasses from "../../components/CardClasses/CardClasses";
 
-
-// const Classes = () => {
-//     const dispatch = useDispatch();
-//     const activities = useSelector((state) => state.activities);
-//     const coaches = useSelector((state) => state.coaches);
-//     // const classes = useSelector((state) => state.classes);
-
-//     useEffect(() => {
-//         dispatch(getActivities());
-//         dispatch(getClassess());
-//         dispatch(getCoaches());
-//     }, [dispatch]);
-
-//     return (
-//         <div>
-//             <br />
-//             <h1>Clases</h1>
-//             <div>
-//                 {activities?.map((activity) => {
-//                     return activity.Classes.map((clase, index) => {
-//                         const coach = coaches.find(coach => coach.id === clase.Coach.id);
-//                         const coachName = coach ? `${coach.firstName} ${coach.lastName}` : '';
-//                         return clase.Events.map((event, index) => (
-//                             <CardClasses
-//                                 key={index}
-//                                 id={activity.id}
-//                                 title={activity.title}
-//                                 coachName={coachName}
-//                                 difficulty={clase.difficulty}
-//                                 date={event.date}
-//                                 startTime={event.startTime}
-//                                 endTime={event.endTime}
-//                                 duration={event.duration}
-//                                 quota={clase.quota}
-//                             />
-//                         ));
-//                     });
-//                 })}
-//             </div>
-//         </div>
-//     )
-
-// };
-
 const Classes = () => {
     const dispatch = useDispatch();
     const activities = useSelector((state) => state.activities);
     const coaches = useSelector((state) => state.coaches);
-    // const classes = useSelector((state) => state.classes);
+    const classes = useSelector((state) => state.classes);
 
     useEffect(() => {
         dispatch(getActivities());
@@ -68,29 +24,26 @@ const Classes = () => {
             <br />
             <h1>Clases</h1>
             <br />
-            {/* <div> */}
-                {activities?.map((activity) => {
-                    return activity.Classes.map((clase, index) => {
-                        const coach = coaches.find(coach => coach.id === clase.Coach.id);
-                        const coachName = coach ? `${coach.firstName} ${coach.lastName}` : '';
-                        return (
-                            <CardClasses
-                                key={index}
-                                id={activity.id}
-                                title={activity.title}
-                                difficulty={clase.difficulty}
-                                startTime={clase.Events[0].startTime}
-                                endTime={clase.Events[0].endTime}
-                                duration={clase.Events[0].duration}
-                                quota= {clase.quota}
-                                date={clase.Events.map((d) => d.date)}
-                                coachName={coachName}
-                            />
-                        );
-                    });
-                })}
-            </div>
-        // </div>
+            {classes?.map((clase,index) => {
+                const activity = activities.find(act => act.id === clase.ActivityId);
+                const activitieName = activity ? `${activity.title}` : '';
+                const coach = coaches.find((coach) => coach.id === clase.CoachId);
+                const coachName = coach ? `${coach.firstName} ${coach.lastName}` : '';
+                return (
+                    <CardClasses
+                    key={index}
+                    title={activitieName}
+                    difficulty={clase.difficulty}  //classes
+                    startTime={clase.Events[0].startTime} //classes
+                    endTime={clase.Events[0].endTime} //classes
+                    duration={clase.Events[0].duration} //classes
+                    quota= {clase.quota}
+                    date={clase.Events.map((d) => d.date)}
+                    coachName={coachName}
+                    />
+                );
+            })}
+        </div>
     )
 };
 

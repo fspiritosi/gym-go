b91013fca6b6a28cc3e5cc8d3102f6cc227877de
Dac1977 <diegodac77@gmail.com>
2023-07-15 09:22:39 -0300

    ejemplo de utilizacion del compponente en App

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import Landing from "./views/Landing/Landing";
 import Detail from "./views/Detail/Detail";
 import Nav from "./components/Nav/Nav";
 import FormCreateActivities from "./views/FormCreateActivities/FormCreateActivities";
-import Profesores from './views/Profesores/Profesores'; //Se volvio a agregar la ruta profesores por que no estaba
-import PaquetesClases from './views/PaquetesClases/PaquetesClases'
+import Profesores from "./views/Profesores/Profesores"; //Se volvio a agregar la ruta profesores por que no estaba
+import PaquetesClases from "./views/PaquetesClases/PaquetesClases";
 import FormGoals from "./views/FormGoals/FormGoals";
 import Footer from "./components/Footer/Footer";
 import FormClasses from "./views/FormClasses/FormClasses";
 import ActivitiesV from "./views/ActivitiesV/ActivitiesV";
 import Admin from "./views/Admin/Admin";
-import Dashboard from "./views/Admin/scenes/Dashboard/Dashboard"
+import Dashboard from "./views/Admin/scenes/Dashboard/Dashboard";
 import Users from "./views/Admin/scenes/Users/Users";
 import Activities from "./views/Admin/scenes/Tables/ActivitiesData";
 import Coaches from "./views/Admin/scenes/Tables/CoachesData";
@@ -21,6 +21,7 @@ import Classes from "./views/Classes/Classes";
 
 import axios from "axios";
 import CreateGoals from "./views/Admin/scenes/Forms/createGoals";
+import { AuthenticationGuard } from "./components/Protected-route/authentication-guard";
 axios.defaults.baseURL = "http://localhost:3001";
 // axios.defaults.baseURL = "https://gym-go-production.up.railway.app"
 
@@ -39,6 +40,11 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/activities" element={<ActivitiesV />} />
+        {/* este es un ejemplo de como proteger las rutas con el componente AutenticationGuard */}
+        {/* <Route
+          path="/activities"
+          element={<AuthenticationGuard component={<ActivitiesV />} />}
+        /> */}
         <Route path="/activity-detail/:id" element={<Detail />} />
         <Route path="/create-activity" element={<FormCreateActivities />} />
         <Route path="/create-goals" element={<FormGoals />} />

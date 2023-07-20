7bf97b2d0c3f101a231f5dbdb4c6a1c826c1f05d
Oscar Velazquez <oscar.industial@gmail.com>
2023-07-20 19:49:15 -0300

    modificacion de la card paquetes

--- client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
+++ client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
@@ -4,9 +4,9 @@ import MercadoPago from '../MercadoPago/MercadoPago';
 import Modal from 'react-modal';
 import { useAuth0 } from '@auth0/auth0-react';
 import 'react-toastify/dist/ReactToastify.css'; //toastify
-import backgroundImage from '../../Assets/backgraund/Fondo2.jpg'
-import card from '../../Assets/backgraund/card.jpg'
-import Logo from "../../Assets/Logos/Logo.svg";
+import backgroundImage from '../../assets/backgraund/cardgymgo.jpg'
+import card from '../../assets/backgraund/card.jpg'
+import Logo from "../../assets/Logos/Logo.svg";
 import {FaWindowClose} from 'react-icons/fa'
 
 
@@ -46,12 +46,10 @@ const CardPaquete = ({ title, clases, price, comprar, description }) => {
 
     return (
        
-        <div class="px-6 py-4 bg-gray hover:shadow-gray-claro rounded-lg shadow-md">
-                    <p class="text-xl font-semibold">{title}</p>
-                    <h4 class="mt-2 text-4xl font-semibol"> <span className=' font-bold text-5xl text-green'>$</span>{price} <span class="text-base font-normal text-gray-600 dark:text-gray-400">/ Año </span></h4>
-                    <p class="mt-4 text-lg">{description}</p>
-
-                   <button onClick={handleBuy} class=" text-sm w-40 text-white bg-green-neon hover:bg-green focus:ring-4 focus:ring-green-neon font-medium rounded-lg text-s px-5 py-2.5 text-center" type="button" data-modal-toggle="authentication-modal">
+        <div class="bg-cover px-6 py-12  bg-gray hover:shadow-gray-claro rounded-lg shadow-md" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                    <p class="text-xl font-semibold text-black">{title}</p>
+                    <h4 class="mt-2 text-black text-4xl font-semibol"> <span className=' font-bold text-5xl text-green'>$</span>{price} <span class="text-base font-normal text-black dark:text-gray-40">/ Año </span></h4>
+                   <button onClick={handleBuy} class=" text-sm w-40 text-black bg-green-neon hover:bg-green focus:ring-4 focus:ring-green-neon font-medium rounded-lg text-s px-5 py-2.5 text-center" type="button" data-modal-toggle="authentication-modal">
                         Elegir
                     </button>
                     {showMercadoPago && <MercadoPago orderData={orderData} />}

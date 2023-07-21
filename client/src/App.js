cdd1a4e98465534e7ff42da589d6c1f43474872e
fspiritosi <fspiritosi@gmail.com>
2023-07-20 21:31:29 -0300

    cambiando los accespoint

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ import CreateActivitie from "./views/Admin/scenes/Forms/createActivities";
 import UserProfile from "./views/UserProfile/UserProfile";
 import Sales from "./views/Admin/scenes/Tables/SalesData";
 
-axios.defaults.baseURL = "http://localhost:3001";
-//axios.defaults.baseURL = "https://gym-go-production.up.railway.app";
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://gym-go-production.up.railway.app";
 
 function App() {
   let location = useLocation();

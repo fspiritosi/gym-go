3842a823ae674d0bae0da2d304c3b48d6263a990
Oscar Velazquez <oscar.industial@gmail.com>
2023-07-20 22:47:05 -0300

    arreglo 2

--- client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
+++ client/src/components/CardPaqueteClase/CardPaqueteClase.jsx
@@ -4,9 +4,9 @@ import MercadoPago from '../MercadoPago/MercadoPago';
 import Modal from 'react-modal';
 import { useAuth0 } from '@auth0/auth0-react';
 import 'react-toastify/dist/ReactToastify.css'; //toastify
-import backgroundImage from '../../assets/backgraund/cardgymgo.jpg'
-import card from '../../assets/backgraund/card.jpg'
-import Logo from "../../assets/Logos/Logo.svg";
+import backgroundImage from '../../Assets/backgraund/cardgymgo.jpg'
+import card from '../../Assets/backgraund/card.jpg'
+import Logo from "../../Assets/Logos/Logo.svg";
 import {FaWindowClose} from 'react-icons/fa'
 
 
@@ -46,12 +46,14 @@ const CardPaquete = ({ title, clases, price, comprar, description }) => {
 
     return (
        
-        <div class="bg-cover px-6 py-12  bg-gray hover:shadow-gray-claro rounded-lg shadow-md" style={{ backgroundImage: `url(${backgroundImage})` }}>
-                    <p class="text-xl font-semibold text-black">{title}</p>
+        <div class="flex justify-end items-center bg-cover aspect-video  bg-gray hover:shadow-gray-claro rounded-lg shadow-md" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                  <div class="mr-16">
+                    <p class="  text-3xl font-semibold text-black">{title}</p>
                     <h4 class="mt-2 text-black text-4xl font-semibol"> <span className=' font-bold text-5xl text-green'>$</span>{price} <span class="text-base font-normal text-black dark:text-gray-40">/ Año </span></h4>
                    <button onClick={handleBuy} class=" text-sm w-40 text-black bg-green-neon hover:bg-green focus:ring-4 focus:ring-green-neon font-medium rounded-lg text-s px-5 py-2.5 text-center" type="button" data-modal-toggle="authentication-modal">
                         Elegir
                     </button>
+                  </div>
                     {showMercadoPago && <MercadoPago orderData={orderData} />}
                     
                     <div class="hidden overflow-x-hidden overflow-y-auto fixed h-modal md:h-full top-4 left-0 right-0 md:inset-0 z-50 justify-center items-center">
